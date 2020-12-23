120a720b0849fa298a627287550be6ad03ce3c18
ryan <4721319+rpshukla@users.noreply.github.com>
2020-12-23 15:56:07 -0700

    Initial commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,48 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+HarmonizerjuceAudioProcessorEditor::HarmonizerjuceAudioProcessorEditor (HarmonizerjuceAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+
+    addAndMakeVisible(pitch);
+    pitch.setColour(Label::ColourIds::textColourId, Colours::white);
+    pitch.setFont(50.0f);
+    pitch.setJustificationType(Justification::centred);
+    pitch.setText("pitch", NotificationType::dontSendNotification);
+}
+
+HarmonizerjuceAudioProcessorEditor::~HarmonizerjuceAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void HarmonizerjuceAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setColour (Colours::white);
+    g.setFont (15.0f);
+}
+
+void HarmonizerjuceAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+    pitch.setBounds (40, 30, 100, getHeight() - 60);
+}
